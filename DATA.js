7c5f8adc83631b6cc84ae2691862c028c9e29daa
thecoder.live <82015917+thecoderlive@users.noreply.github.com>
2023-06-20 10:06:16 +0530

    Initial Commit for file: DATA.js

---
+++ DATA.js
@@ -0,0 +1,145 @@
+export const item = {
+    "id": "d8e5ebc6-7863-4860-98c9-b87eea076f9b",
+    "home_feed": [
+        {
+            "id": "bc0f7a19-61d4-43ab-8bbd-3c99e272fed7",
+            "artist_name": "animal",
+            "home_carousel": [
+                {
+                    "id": "55bd1d96-2995-40c0-a7fb-af452520f787",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/161.png"
+                },
+                {
+                    "id": "9b0e8a7d-f1d8-4fa6-834f-9d44f01c08e7",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/203.png"
+                },
+                {
+                    "id": "e4a6a423-8a51-41d8-87d8-5ecc8e9d101f",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/163.png"
+                },
+                {
+                    "id": "31defc85-a087-425b-81b4-4859b32f98bc",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/206.png"
+                },
+                {
+                    "id": "550fcd02-cfba-4734-aa68-7b48970d4be3",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/20.png"
+                }
+            ],
+            "art_caption": "visit",
+            "art_description": "Down up crime perhaps remain interest national tonight. Final record common hand. Deal call knowledge now president lawyer."
+        },
+        {
+            "id": "807a3853-7382-4778-b8f2-3b8faa6348f3",
+            "artist_name": "red",
+            "home_carousel": [
+                {
+                    "id": "3b9c2be4-7fef-47dd-8df4-2427e2a73cf5",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/131.png"
+                },
+                {
+                    "id": "9fc52d29-6e0d-4774-b07a-fc9b08d4145a",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/200.png"
+                },
+                {
+                    "id": "593f44bc-0bb8-4298-9cfb-6ffcd7c48ba3",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/111.png"
+                },
+                {
+                    "id": "0af8f721-4cbd-4a17-8acb-9ab4d06ecbae",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/247.png"
+                },
+                {
+                    "id": "ae88bdda-cbb1-435d-92d1-c49330bec076",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/45.png"
+                }
+            ],
+            "art_caption": "same",
+            "art_description": "Blue control many prepare return military do culture. Whatever thought cultural adult feeling television become option. From adult whom deal use want nor."
+        },
+        {
+            "id": "5d4409c8-9c0a-445d-9168-633ce01aff6f",
+            "artist_name": "child",
+            "home_carousel": [
+                {
+                    "id": "00194a4e-fa55-42d0-9b3e-739c6c8ea678",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/328.png"
+                },
+                {
+                    "id": "f752212f-b82b-4a74-8d49-392d4e08a567",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/109.png"
+                },
+                {
+                    "id": "3444d211-202f-469f-965a-7df144c32426",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/343.png"
+                },
+                {
+                    "id": "6f9db6e2-e675-4c54-9963-4e586e9e8f7f",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/124.png"
+                },
+                {
+                    "id": "70008080-a40a-42d4-9206-18347d5cdfeb",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/155.png"
+                }
+            ],
+            "art_caption": "something",
+            "art_description": "Of vote compare fill summer role movie. Television across hair computer. Tell agent like special forget."
+        },
+        {
+            "id": "9e677317-0ba1-42bf-8869-e706d5060d13",
+            "artist_name": "group",
+            "home_carousel": [
+                {
+                    "id": "666e586d-ab51-4b8b-b663-28bffcfcc4e8",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/178.png"
+                },
+                {
+                    "id": "0388fb8c-f2de-40bc-90ea-b70d345c68bb",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/29.png"
+                },
+                {
+                    "id": "13883247-0552-4739-a889-bab5cd2acf99",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/191.png"
+                },
+                {
+                    "id": "1e5c4b95-970e-4685-96a8-3d41e7fe9731",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/313.png"
+                },
+                {
+                    "id": "d45a991b-9ae3-47d3-87fe-f6c4e2601577",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/147.png"
+                }
+            ],
+            "art_caption": "true",
+            "art_description": "Arm school measure become last cell. Just fill peace whom. Particularly pull feel watch discuss. Civil city education probably."
+        },
+        {
+            "id": "06c7239a-a0ba-49d8-a05e-0cbed82b6783",
+            "artist_name": "end",
+            "home_carousel": [
+                {
+                    "id": "8c637ae0-f177-49ed-8a71-e78b03be5f0c",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/167.png"
+                },
+                {
+                    "id": "dba137e1-39d6-40fc-8138-3fea61d4c8ca",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/298.png"
+                },
+                {
+                    "id": "95df7818-d36c-4170-92df-e9ed37a35101",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/141.png"
+                },
+                {
+                    "id": "3c0fd026-13d8-4148-97fd-ac95773a2c11",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/323.png"
+                },
+                {
+                    "id": "02a048a6-a497-4d04-bdf2-cba09a090e3a",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/243.png"
+                }
+            ],
+            "art_caption": "sister",
+            "art_description": "Particularly player data major last stand. Section trip evening area order. Practice season get one bar together goal behind."
+        }
+    ]
+}
